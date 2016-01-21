d786fd7a1fe68b6ed7311aa703add3c8c654f160
Karim Abou Zeid <karim23697@gmail.com>
2016-01-21 01:48:11 +0100

    Fixed lag while scrolling through large lists

--- sample/src/main/java/com/simplecityapps/recyclerview_fastscroll/sample/activity/MainActivity.java
+++ sample/src/main/java/com/simplecityapps/recyclerview_fastscroll/sample/activity/MainActivity.java
@@ -58,7 +58,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public int getItemCount() {
-            return 20;
+            return 200;
         }
 
         @Override
