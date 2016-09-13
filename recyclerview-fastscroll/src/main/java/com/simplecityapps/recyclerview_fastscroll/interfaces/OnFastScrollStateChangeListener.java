7868773003c095165bde52b61adcd74283368e56
Tim Malseed <t.malseed@gmail.com>
2016-09-13 20:30:51 +1000

    Used javadoc for state change interface method descriptions

--- recyclerview-fastscroll/src/main/java/com/simplecityapps/recyclerview_fastscroll/interfaces/OnFastScrollStateChangeListener.java
+++ recyclerview-fastscroll/src/main/java/com/simplecityapps/recyclerview_fastscroll/interfaces/OnFastScrollStateChangeListener.java
@@ -2,9 +2,13 @@ package com.simplecityapps.recyclerview_fastscroll.interfaces;
 
 public interface OnFastScrollStateChangeListener {
 
-    // Called when fast scrolling begins
+    /**
+     * Called when fast scrolling begins
+     */
     void onFastScrollStart();
 
-    // Called when fast scrolling ends
+    /**
+     * Called when fast scrolling ends
+     */
     void onFastScrollStop();
 }
